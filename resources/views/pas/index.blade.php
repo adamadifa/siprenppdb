655e9373022af20a819f29f05071c0d9496d5dda
Adam Adifa <adamabdi.al.a@gmail.com>
2023-03-01 20:02:29 +0700

    update

--- resources/views/pas/index.blade.php
+++ resources/views/pas/index.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
-    <title>Penerimaan Santri Baru 2022-2023 - Persis Al Amin Sindangkasih</title>
+    <title>Download Sertifikat PAS Got Talent</title>
     <link rel="apple-touch-icon" href="{{asset('app-assets/images/ico/apple-icon-120.png')}}">
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('app-assets/images/ico/favicon.ico')}}">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
