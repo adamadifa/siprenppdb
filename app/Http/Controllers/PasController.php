f7a4c8a998ea4cac530b4108c83200ff8c33f126
Adam Adifa <adamabdi.al.a@gmail.com>
2023-03-01 20:09:16 +0700

    updatE

--- app/Http/Controllers/PasController.php
+++ app/Http/Controllers/PasController.php
@@ -10,7 +10,7 @@ class PasController extends Controller
     public function index(Request $request)
     {
         if (!empty($request->nama_peserta)) {
-            $peserta = DB::table('sertifikat')->where('nama_peserta', 'like', '%' . $request->nama_peserta . '%')->get()->orderBy('nama_peserta');
+            $peserta = DB::table('sertifikat')->where('nama_peserta', 'like', '%' . $request->nama_peserta . '%')->orderBy('nama_peserta')->get();
         } else {
             $peserta = DB::table('sertifikat')->orderBy('nama_peserta')->get();
         }
